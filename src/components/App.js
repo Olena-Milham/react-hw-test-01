aabcbfb17b4ffab342cfbd223b0692f70e540579
Olena <nikimih@gmail.com>
2023-02-13 15:47:48 +0000

    update data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import Painting from './Painting';
-import paintings from './paintings.json'
+import paintings from '../paintings.json'
 
 export default function App () {
   return (<div>
     <Painting
     imageUrl={paintings[0].url}
     title={paintings[0].title}
-    author = {paintings[0].author}
+    author = {paintings[0].author.tag}
     profile ={paintings[0].profile}
     price = {paintings[0].price}
     />
--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,16 +1,25 @@
-
+import PropTypes from 'prop-types'
 const Painting =({imageUrl, title, profile, author, price})=>{
     return(
         <div>
             <img src={imageUrl} alt={title} width="480"/>
             <h2>{title}</h2>
             <p>
-                Author: <a href={profile}> {author} </a>
+                Author: <a href={profile}> {author.tag} </a>
             </p>
             <p> Price: {price} credits</p>
             <p> Availability: out of stock or available </p>
 <button type ='button'> Add to cart </button>
         </div>
     )
+};
+Painting.propTypes={
+    imageUrl: PropTypes.string,
+    title: PropTypes.string,
+    profile: PropTypes.string,
+    author:PropTypes.string,
+    price:PropTypes.number,
+
+
 }
 export default Painting;
