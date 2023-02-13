5eb5119bee1b848036f31e09764aefeda3e5e761
Olena <nikimih@gmail.com>
2023-02-13 17:50:34 +0000

    add collection (list)

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,10 @@
-import Painting from './Painting';
+import PaintingList from './PaintingList'
 import paintings from '../paintings.json'
 
 export default function App () {
   return (<div>
     {paintings.map(painting=>( 
-    <Painting
-    key={painting.id}
-    imageUrl={painting.url}
-    title={painting.title}
-    author = {painting.author.tag}
-    profile ={painting.author.url}
-    price = {painting.price}
-    quantity={painting.quantity}
-    />
+    <PaintingList cards={paintings}/>
     ))}
-   
     </div>)
-
 };
--- src/components/PaintingList.js
+++ src/components/PaintingList.js
@@ -1,9 +1,22 @@
-function PaintingList ({cards}) {
-    <ul>
-        {cards.map(card=>{
-            <li key = {card.id}>
 
-            </li>
-        })}
-    </ul>
+import Painting from './Painting'
+function PaintingList ({cards}) {
+   return (
+    <ul>
+    {cards.map(card=>(
+        <li key={card.id}>
+            <Painting    
+imageUrl={card.url}
+title={card.title}
+author = {card.author.tag}
+profile ={card.author.url}
+price = {card.price}
+quantity={card.quantity}
+/>
+
+        </li>
+    ))}
+</ul>
+   );
 }
+export default PaintingList;
